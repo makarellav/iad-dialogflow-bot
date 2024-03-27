438d2d9448b338ebd4cbe98eb9f17164b00d8ed0
makarellav <makarellads@gmail.com>
2024-03-27 15:41:36 +0200

    add a lot of stuff

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/makarellav/iad-dialogflow-bot
 
 go 1.22
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	cloud.google.com/go/dialogflow v1.51.0 // indirect
+	cloud.google.com/go/longrunning v0.5.5 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311132316-a219d84964c2 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240311132316-a219d84964c2 // indirect
+	google.golang.org/grpc v1.62.1 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/joho/godotenv"
+	"io"
 	"log"
 	"net/http"
-	"os"
 )
+
+type Coin struct {
+	Id                string `json:"id"`
+	Rank              string `json:"rank"`
+	Symbol            string `json:"symbol"`
+	Name              string `json:"name"`
+	Supply            string `json:"supply"`
+	MaxSupply         string `json:"maxSupply"`
+	MarketCapUsd      string `json:"marketCapUsd"`
+	VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
+	PriceUsd          string `json:"priceUsd"`
+	ChangePercent24Hr string `json:"changePercent24Hr"`
+	Vwap24Hr          string `json:"vwap24Hr"`
+}
+
+type CoinData struct {
+	Data Coin `json:"data"`
+}
 
 type intent struct {
 	DisplayName string `json:"displayName"`
 }
 
 type queryResult struct {
-	Intent intent `json:"intent"`
+	Intent     intent            `json:"intent"`
+	Action     string            `json:"action,omitempty"`
+	Parameters map[string]string `json:"parameters,omitempty"`
 }
 
 type text struct {
@@ -73,6 +92,68 @@ func getAgentName(request webhookRequest) (webhookResponse, error) {
 	return response, nil
 }
 
+func getCurrencyPrice(request webhookRequest) (webhookResponse, error) {
+	currency, ok := request.QueryResult.Parameters["currency"]
+	property, ok := request.QueryResult.Parameters["property"]
+
+	if !ok {
+		fmt.Println("not found")
+		return webhookResponse{}, nil
+	}
+
+	req, err := http.NewRequest("GET", "https://api.coincap.io/v2/assets/"+currency, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return webhookResponse{}, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return webhookResponse{}, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return webhookResponse{}, err
+	}
+
+	fmt.Println(string(body))
+
+	var coin CoinData
+
+	err = json.Unmarshal(body, &coin)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return webhookResponse{}, err
+	}
+
+	var response webhookResponse
+
+	if property == "price" {
+		response = webhookResponse{
+			FulfillmentMessages: []message{
+				{
+					Text: text{
+						Text: []string{fmt.Sprintf("%s %s", coin.Data.Name, coin.Data.PriceUsd)},
+					},
+				},
+			},
+		}
+	}
+
+	return response, nil
+}
+
 // handleError handles internal errors.
 func handleError(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusInternalServerError)
@@ -92,14 +173,18 @@ func HandleWebhookRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("Request: %+v", request)
 
+	fmt.Println("?")
+
 	// Call intent handler
 	switch intent := request.QueryResult.Intent.DisplayName; intent {
 	case "Default Welcome Intent":
 		response, err = welcome(request)
 	case "get-agent-name":
 		response, err = getAgentName(request)
+	case "price":
+		response, err = getCurrencyPrice(request)
 	default:
-		err = fmt.Errorf("Unknown intent: %s", intent)
+		err = fmt.Errorf("unknown intent: %s", intent)
 	}
 	if err != nil {
 		handleError(w, err)
@@ -117,21 +202,192 @@ func HandleWebhookRequest(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	err := godotenv.Load()
+	mux.HandleFunc("POST /webhook", HandleWebhookRequest)
 
-	if err != nil {
-		fmt.Println("failed to load env")
-	}
-
-	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
-	})
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "7777"
-	}
-
-	log.Fatal(http.ListenAndServe("0.0.0.0"+port, mux))
+	log.Fatal(http.ListenAndServe(":7777", mux))
 }
+
+type UserSaysData struct {
+	Text  string `json:"text"`
+	Alias string `json:"alias,omitempty"`
+	Meta  string `json:"meta,omitempty"`
+}
+
+type UserSays struct {
+	Data []UserSaysData `json:"data"`
+}
+
+type Intent struct {
+	//Id                        string        `json:"id"`
+	Name string `json:"name"`
+	//Auto                      bool          `json:"auto"`
+	//Condition                 string        `json:"condition"`
+	//ConditionalFollowupEvents []interface{} `json:"conditionalFollowupEvents"`
+	//ConditionalResponses      []interface{} `json:"conditionalResponses"`
+	//Context                   []interface{} `json:"context"`
+	//Contexts                  []interface{} `json:"contexts"`
+	//EndInteraction            bool          `json:"endInteraction"`
+	//Events                    []interface{} `json:"events"`
+	//FallbackIntent            bool          `json:"fallbackIntent"`
+	//LiveAgentHandoff          bool          `json:"liveAgentHandoff"`
+	//ParentId                  interface{}   `json:"parentId"`
+	//FollowUpIntents           []interface{} `json:"followUpIntents"`
+	//Priority                  int           `json:"priority"`
+	//Responses                 []struct {
+	//	Action           string        `json:"action"`
+	//	AffectedContexts []interface{} `json:"affectedContexts"`
+	//	Parameters       []struct {
+	//		NoInputPromptMessages []interface{} `json:"noInputPromptMessages"`
+	//		NoMatchPromptMessages []interface{} `json:"noMatchPromptMessages"`
+	//		PromptMessages        []interface{} `json:"promptMessages"`
+	//		DefaultValue          string        `json:"defaultValue"`
+	//		Name                  string        `json:"name"`
+	//		DataType              string        `json:"dataType"`
+	//		IsList                bool          `json:"isList"`
+	//		Required              bool          `json:"required"`
+	//		Prompts               []interface{} `json:"prompts"`
+	//		Value                 string        `json:"value"`
+	//		OutputDialogContexts  []interface{} `json:"outputDialogContexts"`
+	//	} `json:"parameters"`
+	//	DefaultResponsePlatforms struct {
+	//	} `json:"defaultResponsePlatforms"`
+	//	Messages []struct {
+	//		Type      string   `json:"type"`
+	//		Condition string   `json:"condition"`
+	//		Speech    []string `json:"speech"`
+	//	} `json:"messages"`
+	//	ResetContexts bool `json:"resetContexts"`
+	//} `json:"responses"`
+	//RootParentId interface{}   `json:"rootParentId"`
+	//Templates    []interface{} `json:"templates"`
+	UserSays []UserSays `json:"userSays"`
+	//WebhookForSlotFilling bool `json:"webhookForSlotFilling"`
+	//WebhookUsed           bool `json:"webhookUsed"`
+}
+
+//func main() {
+//resp, err := http.Get("https://api.coincap.io/v2/assets?limit=2000")
+//
+//if err != nil {
+//	fmt.Println("failed to load data", err.Error())
+//
+//	return
+//}
+//
+//data, err := io.ReadAll(resp.Body)
+//
+//if err != nil {
+//	fmt.Println("failed to read data", err.Error())
+//
+//	return
+//}
+//
+//fmt.Println(string(data))
+//
+//var coinsData struct {
+//	Data []Coin `json:"data"`
+//}
+//
+//err = json.Unmarshal(data, &coinsData)
+//
+//if err != nil {
+//	fmt.Println("failed unmarshalling", err.Error())
+//
+//	return
+//}
+//
+////headers := []string{"name", "synonyms"}
+//
+//f, err := os.Create("data.csv")
+//
+//defer f.Close()
+//
+//if err != nil {
+//	fmt.Println(err.Error())
+//
+//	return
+//}
+//
+//w := csv.NewWriter(f)
+//defer w.Flush()
+//
+////w.Write(headers)
+//
+//for _, coin := range coinsData.Data {
+//	w.Write([]string{strconv.Quote(*coin.Id), strconv.Quote(*coin.Id), strconv.Quote(*coin.Name), strconv.Quote(*coin.Symbol)})
+//}
+
+//{
+//	"isTemplate": false,
+//	"data": [
+//{
+//"text": "bitcoin",
+//"userDefined": false,
+//"alias": "currency",
+//"meta": "@currency"
+//},
+//{
+//"text": " цена",
+//"userDefined": false
+//}
+//],
+//"count": 0,
+//"id": "03c9cc35-8ed2-4833-9f96-53b56c5bce5e",
+//"updated": null
+//},
+
+//var currencyPriceIntent Intent
+//currencyPriceIntent.Name = "currency.price"
+//
+//priceValues := []string{"ціна", "цена"}
+//
+//f, err := os.Open("data.csv")
+//defer f.Close()
+//
+//if err != nil {
+//	fmt.Println(err.Error())
+//
+//	return
+//}
+//
+//r := csv.NewReader(f)
+//
+//coins, err := r.ReadAll()
+//
+//if err != nil {
+//	fmt.Println(err.Error())
+//
+//	return
+//}
+//
+//for _, coin := range coins[:len(coins)/2+1] {
+//	for _, priceValue := range priceValues {
+//		var userSaysData UserSays
+//
+//		userSaysCoin := UserSaysData{
+//			Text:  coin[0],
+//			Alias: "currency",
+//			Meta:  "@currency",
+//		}
+//
+//		userSaysPrice := UserSaysData{
+//			Text: " " + priceValue,
+//		}
+//
+//		userSaysData.Data = append(userSaysData.Data, userSaysCoin, userSaysPrice)
+//		currencyPriceIntent.UserSays = append(currencyPriceIntent.UserSays, userSaysData)
+//	}
+//}
+//
+//data, err := json.Marshal(currencyPriceIntent)
+//
+//if err != nil {
+//	fmt.Println(err.Error())
+//
+//	return
+//}
+//
+//fmt.Println(len(currencyPriceIntent.UserSays))
+//
+//log.Fatal(os.WriteFile("price.json", data, 0644))
+//}
